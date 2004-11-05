e8b765576c24a660c3f7c8321dbc9e6b067dad1c
Unknown <woelfl@unknown>
2004-11-05 18:12:58 +0000

    Minor modifications
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@292 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Calculi/Time/AllenHayes/BaseAllenHayes.thy
+++ Calculi/Time/AllenHayes/BaseAllenHayes.thy
@@ -2,7 +2,21 @@
 theory BaseAllenHayes = Main:
 
 
-ML "proofs := 1"
+(*
+  author: Stefan Woelfl
+  date:   05-11-2004
+  
+  Summary: 
+  =======
+  This thy file contains the basics of Allen and Hayes' first order
+  axiomatization of intervals in linear time. 
+  We prove that the basic Meet relation ("M") is irreflexiv, asymetric, and
+  transitive.
+ 
+  The proofs here are just brute force.
+*)
+
+
 
 typedecl "Interval"
 
@@ -11,23 +25,23 @@ consts
 
 
 axioms
-M1: "!! x :: Interval . !! y :: Interval . !! z :: Interval . !! u :: Interval . 
+M1: "!! x y z u :: Interval . 
   (M x y & M x u) & M z y ==> M z u"
-M2a: "!! x :: Interval . !! y :: Interval . !! z :: Interval . !! u :: Interval . 
+M2a: "!! x y z u :: Interval . 
   M x y & M z u ==> (M x u | (? v :: Interval . M x v & M v u)) | (? v :: Interval . M z v & M v y)"
-M2b: "!! x :: Interval . !! y :: Interval . !! z :: Interval . !! u :: Interval . 
-  (M x y & M z u) & M x u ==> (Not (? v :: Interval . M x v & M v u)) & (Not (? v :: Interval . M z v & M v y))"
-M2c: "!! x :: Interval . !! y :: Interval . !! z :: Interval . !! u :: Interval . 
-  (M x y & M z u) & (? v :: Interval . M x v & M v u) ==> (Not (M x u)) & (Not (? v :: Interval . M z v & M v y))"
-M2d: "!! x :: Interval . !! y :: Interval . !! z :: Interval . !! u :: Interval . 
-  (M x y & M z u) & (? v :: Interval . M z v & M v y) ==> (Not (M x u)) & (Not (? v :: Interval . M x v & M v u))"
+M2b: "!! x y z u :: Interval . M x y & M z u & M x u ==> 
+  (Not (? v :: Interval . M x v & M v u)) & (Not (? v :: Interval . M z v & M v y))"
+M2c: "!! x y z u :: Interval . (M x y & M z u) & (? v :: Interval . M x v & M v u) ==> 
+  (Not (M x u)) & (Not (? v :: Interval . M z v & M v y))"
+M2d: "!! x y z u :: Interval . (M x y & M z u) & (? v :: Interval . M z v & M v y) ==> 
+  (Not (M x u)) & (Not (? v :: Interval . M x v & M v u))"
 M3: "!! x :: Interval . ? y :: Interval . ? z :: Interval . M y x & M x z"
-M4: "!! x :: Interval . !! y :: Interval . !! z :: Interval . !! u :: Interval . 
+M4: "!! x y z u :: Interval . 
   ((M x y & M y u) & M x z) & M z u ==> y = z"
 
 
 
-theorem M_irrefl: "!! x :: Interval . (Not (M x x))"
+theorem M_irrefl[intro]: "!! x :: Interval . (Not (M x x))"
 proof -
   fix x
   from M1 M2a M2b M2c M2d M3 M4
@@ -35,7 +49,7 @@ proof -
 qed
 
 
-theorem M_asym[elim]: "!! x :: Interval . !! y :: Interval . M x y ==> (Not (M y x))"
+theorem M_asym[intro]: "!! x :: Interval . !! y :: Interval . M x y ==> (Not (M y x))"
 proof -
   fix x y
   assume "M x y"
@@ -45,7 +59,7 @@ proof -
 qed
 
 
-theorem M_atrans[elim]: "!! x :: Interval . !! y :: Interval . !! z :: Interval . M x y & M y z ==> (Not (M x z))"
+theorem M_atrans[intro]: "!! x :: Interval . !! y :: Interval . !! z :: Interval . M x y & M y z ==> (Not (M x z))"
 proof -
   fix x y z
   assume " M x y & M y z"
