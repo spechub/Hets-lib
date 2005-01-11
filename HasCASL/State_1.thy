7e2d4c8bb3c801a20e9fe60a0bd096d413f4b48f
Unknown <2sonja@unknown>
2005-01-11 17:08:09 +0000

    Specification and proof script files (sonjas diploma thesis)
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@308 9acf5e20-c632-0410-b30d-e55f0e25a569

---
+++ HasCASL/State_1.thy
@@ -0,0 +1,56 @@
+ML "val hetsLib = (OS.Process.getEnv \"HETS_LIB\"); 
+case hetsLib of NONE => add_path \".\" 
+| SOME s => add_path (s ^ \"/Isabelle\")"
+
+theory State_1 = MainHC:
+typedecl "S"
+
+consts
+"GtXGtXEqX" :: "((S => (S * 'a) option) * ('a => (S => (S * 'a) option) option)) => (S => (S * 'a) option) option"   ( "GtXGtXEqX" )
+"return" :: "'a => (S => (S * 'a) option) option"   ( "return" )
+
+
+axioms
+return_def: "app (Some return) (Some x) = (Some (% s . pair (Some s) (Some x)))"
+bind_def: "app (Some GtXGtXEqX) (pair (Some m) (Some f)) = (Some (% s . app (Some (% (s', a) . app (app (Some f) (Some a)) (Some s'))) (app (Some m) (Some s))))"
+
+lemmas return_def' = return_def [simplified]
+lemmas bind_def' = bind_def [simplified]
+
+declare return_def' [simp]
+declare return_def [simp]
+declare bind_def' [simp]
+declare bind_def [simp]
+
+
+theorem monad_assoc: "!! x :: 'a . app (Some GtXGtXEqX) (pair (app (Some GtXGtXEqX) (pair (app (Some f) (Some x)) (Some g))) (Some h)) = app (Some GtXGtXEqX) (pair (app (Some f) (Some x)) (Some (% y . app (Some GtXGtXEqX) (pair (app (Some g) (Some y)) (Some h)))))"
+apply (simp)
+apply (case_tac "f x")
+apply (simp_all)
+apply (rule ext)
+apply (case_tac "a s")
+apply (auto)
+apply (case_tac "g b")
+apply (simp_all)
+done
+theorem monad_unit1: "app (Some GtXGtXEqX) (pair (app (Some f) (Some x)) (Some return)) = app (Some f) (Some x)"
+apply (simp)
+apply (case_tac "f x")
+apply (simp_all)
+apply (rule ext)
+apply (case_tac "a s")
+apply (simp_all)
+done
+theorem monad_unit2a: "(defOp (app (Some f) (Some x))) --> app (Some GtXGtXEqX) (pair (app (Some return) (Some x)) (Some f)) = app (Some f) (Some x)"
+apply (simp)
+apply (case_tac "f x")
+apply (simp_all)
+done
+theorem monad_unit2b: "app (Some GtXGtXEqX) (pair (Some m) (Some (% x . app (Some GtXGtXEqX) (pair (app (Some return) (Some x)) (Some f))))) = app (Some GtXGtXEqX) (pair (Some m) (Some f))"
+apply (simp)
+apply (rule ext)
+apply (case_tac "m s")
+apply (simp_all)
+done
+
+end
