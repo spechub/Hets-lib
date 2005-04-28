2f94b1581ed600f8f69ef54bbe328393f2333f23
Christian Maeder <Christian.Maeder@dfki.de>
2005-04-28 08:56:31 +0000

    omit mixfix annotation
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@387 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/Functions_1.thy
+++ HasCASL/Functions_1.thy
@@ -4,12 +4,12 @@ case hetsLib of NONE => add_path \".\"
 
 theory Functions_1 = MainHC:
 consts
-"id" :: "'a => 'a option"   ( "id" )
-"o" :: "(('b => 'c option) * ('a => 'b option)) => ('a => 'c option)"   ( "o" )
+"id" :: "'a => 'a option"  
+"o_X" :: "(('b => 'c option) * ('a => 'b option)) => ('a => 'c option)" 
 
 
 axioms
-o_def: "apt (Some o) (pair (Some f) (Some g)) = (Some (% x . app (Some f) (app (Some g) (Some x))))"
+o_def: "apt (Some o_X) (pair (Some f) (Some g)) = (Some (% x . app (Some f) (app (Some g) (Some x))))"
 id_def: "(Some id) = (Some (% x . (Some x)))"
 
 lemmas o_def' = o_def [simplified]
@@ -21,16 +21,16 @@ declare id_def' [simp]
 declare id_def [simp]
 
 
-theorem o_assoc: "apt (Some o) (pair (Some f) (apt (Some o) (pair (Some g) (Some h)))) = apt (Some o) (pair (apt (Some o) (pair (Some f) (Some g))) (Some h))"
+theorem o_assoc: "apt (Some o_X) (pair (Some f) (apt (Some o_X) (pair (Some g) (Some h)))) = apt (Some o_X) (pair (apt (Some o_X) (pair (Some f) (Some g))) (Some h))"
 apply (simp)
 apply (rule ext)
 apply (case_tac "h x")
 apply (simp_all)
 done
-theorem id_neut: "apt (Some o) (pair (Some f') (Some id)) = (Some f')"
+theorem id_neut: "apt (Some o_X) (pair (Some f') (Some id)) = (Some f')"
 apply (simp)
 done
-theorem inj: "apt (Some o) (pair (Some f') (Some f')) = (Some id) ==> (! x :: 'a . ! y :: 'a . app (Some f') (Some x) = app (Some f') (Some y) --> (Some x) = (Some y))"
+theorem inj: "apt (Some o_X) (pair (Some f') (Some f')) = (Some id) ==> (! x :: 'a . ! y :: 'a . app (Some f') (Some x) = app (Some f') (Some y) --> (Some x) = (Some y))"
 apply (auto)
 apply (subgoal_tac "option_case None f' (f' y) = Some y")
 apply (drule_tac x = "x" in fun_cong)
@@ -40,7 +40,7 @@ apply (simp)
 apply (case_tac "f' y")
 apply (auto)
 done
-theorem surj: "apt (Some o) (pair (Some f') (Some f')) = (Some id) --> (! x :: 'a . ? y :: 'a . app (Some f') (Some y) = (Some x))"
+theorem surj: "apt (Some o_X) (pair (Some f') (Some f')) = (Some id) --> (! x :: 'a . ? y :: 'a . app (Some f') (Some y) = (Some x))"
 apply (auto)
 apply (rule_tac x = "the (f' x)" in exI)
 apply (drule_tac x = "x" in fun_cong)
