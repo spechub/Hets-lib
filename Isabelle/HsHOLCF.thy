4a50c680b74b9ed2e7c9852dc3ecfaf8270abbff
Christian Maeder <Christian.Maeder@dfki.de>
2005-06-28 16:45:07 +0000

    added Eq and Ord instances
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@435 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/HsHOLCF.thy
+++ Isabelle/HsHOLCF.thy
@@ -1,7 +1,12 @@
+
 theory HsHOLCF = HOLCF:
 
-constdefs
+axclass Eq < pcpo
+(* eq_adm: "adm (% (x,y). x = y)" *)
+axclass Ord < pcpo
+
+constdefs 
 fliftbin :: "(('a::type) => 'a => 'a) => ('a lift -> 'a lift -> 'a lift)"
-"fliftbin f == (LAM y. (LAM x. (((flift1 (%u. (flift2 (%v. f v u))))$x)$y)))"
+"fliftbin f == (LAM y. (LAM x. (((flift1 (%u. (flift2 (%v. f v u))))$x)$y)))"  
 
 end
