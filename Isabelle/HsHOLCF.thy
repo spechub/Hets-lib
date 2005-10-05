7637f675f0fd7f4196e45c2d4357c51e23a8f384
Christian Maeder <Christian.Maeder@dfki.de>
2005-10-05 08:49:56 +0000

    Paolo's update
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@457 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/HsHOLCF.thy
+++ Isabelle/HsHOLCF.thy
@@ -1,5 +1,5 @@
 
-theory HsHOLCF = HOLCF + Seq:
+theory HsHOLCF = HOLCF:
 
 types 
 dInt = "int lift"
@@ -8,8 +8,15 @@ axclass Ord < pcpo
 axclass Eq < pcpo
 (* eq_adm: "adm (% (x,y). x = y)" *)
 
+instance lift :: (type) Eq
+by intro_classes 
+
 constdefs 
 fliftbin :: "(('a::type) => ('b::type) => ('c::type)) => ('a lift -> 'b lift -> 'c lift)"
 "fliftbin f == (LAM y. (LAM x. (((flift1 (%u. (flift2 (%v. f v u))))$x)$y)))"  
+
+(* a type of lazy lists (from Fixrec_ex) *)
+domain 'a llist = lNil | lCons (lazy lHd ::'a) (lazy lTl ::"'a llist")
+
 
 end
