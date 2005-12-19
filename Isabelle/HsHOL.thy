5bec35636cddf6bad94bdba58e8c2175cb9def1d
Christian Maeder <Christian.Maeder@dfki.de>
2005-12-19 11:25:02 +0000

    Paolo's final version
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@505 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/HsHOL.thy
+++ Isabelle/HsHOL.thy
@@ -4,4 +4,17 @@ theory HsHOL = Main:
 axclass Ord < type
 axclass Eq < type
 
+instance bool :: Ord
+by intro_classes
+instance bool :: Eq
+by intro_classes
+instance int :: Ord
+by intro_classes
+instance int :: Eq
+by intro_classes
+instance list :: (Ord) Ord 
+by intro_classes
+instance list :: (Eq) Eq
+by intro_classes
+
 end
