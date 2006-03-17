ea4c9bfee6e1a727e7aa41cdfa58e3bef7cdd79c
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2006-03-17 17:18:24 +0000

    *** empty log message ***
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@589 9acf5e20-c632-0410-b30d-e55f0e25a569

---
+++ ConstraintCASL/RCC8.lisp
@@ -0,0 +1,19 @@
+(defparameter *RCC8* 
+  (def-calculus "Relative distance calculus (reldistcalculus)"
+    :arity :binary
+    :identity-relation __EQ__
+    :inverse-relation ((NTPPi . NTPP)
+      ( . )
+      ( . ))
+
+    :base-relations (__DC__ __EC__ __PO__ __NTPP__ __TPP__ ...)
+    :composition-relation ((DC EC (DC EC PO TPP NTPP))
+  (same closer (same closer farther))
+  (same farther (same closer farther))
+  (closer same (same closer farther))
+  (closer closer (same closer farther))
+  (closer farther (same closer farther))
+  (farther same (same closer farther))
+  (farther closer (same closer farther))
+  (farther farther (same closer farther)))))
+
