c09550d980b31e03dc34d4129aab1a1dfd1e67ef
Christian Maeder <Christian.Maeder@dfki.de>
2006-04-21 08:59:55 +0000

    translation of Eq class changed
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@599 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/HsHOL.thy
+++ Isabelle/HsHOL.thy
@@ -1,20 +1,89 @@
 
 theory HsHOL = Main:
 
+axclass hskTerm < type
+
 axclass Ord < type
 axclass Eq < type
 
-instance bool :: Ord
-by intro_classes
+consts
+hEq :: "('a::Eq) => 'a => bool"
+hNEq :: "('a::Eq) => 'a => bool"
+
+instance "*" :: ("hskTerm","hskTerm") hskTerm ..
+
+instance bool :: hskTerm
+by intro_classes 
+instance int :: hskTerm
+by intro_classes 
 instance bool :: Eq
 by intro_classes
-instance int :: Ord
+instance list :: ("Eq") Eq
 by intro_classes
 instance int :: Eq
 by intro_classes
-instance list :: (Ord) Ord 
-by intro_classes
-instance list :: (Eq) Eq
-by intro_classes
+
+instance list :: ("hskTerm") hskTerm ..
+
+(*
+axioms 
+
+ax1: "hEq p p == Some true"
+
+ax2: "[| hEq q r = Some true; hEq p q = Some true|] ==> hEq p r = Some true"
+
+ax3: "hEq p q = Some true == hNEq p q = Some false"
+*)
+
+axioms 
+
+ax1: "hEq p p"
+
+ax2: "[| hEq q r; hEq p q |] ==> hEq p r"
+
+ax3: "hEq p q == ~ hNEq p q"
 
 end
+
+
+(*
+
+types 
+ opBool = "bool option"
+ opInt = "int option"
+
+constdefs 
+opAnd :: "opBool => opBool => opBool"
+"opAnd a b == case a of 
+   None => None 
+   | Some x => case b of 
+      None => None 
+      | Some y => Some (x & y)"
+
+constdefs
+opOr :: "opBool => opBool => opBool"
+"opOr a b == case a of 
+   None => case b of
+      None => None
+      | Some y => b
+   | Some x => case b of 
+      None => a
+      | Some y => Some (x | y)"
+
+constdefs
+fopbin :: "(('a::type) => ('b::type) => ('c::type)) => ('a option => 'b option => 'c option)"
+"fopbin f a b == case a of 
+   None => None
+   | Some x => case b of 
+      None => None
+      | Some y => Some (f x y)"
+
+opTrue :: opBool 
+"opTrue == Some True"
+
+opFalse :: opBool 
+"opFalse == Some False"
+
+
+end
+*)
