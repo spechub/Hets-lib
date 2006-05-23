5c05689a4546952e8ebbc04662cf931f0dc94a0b
Christian Maeder <Christian.Maeder@dfki.de>
2006-05-23 14:47:27 +0000

    added more definition, rewrote primrecs using constdefs
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@608 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/MainHC.thy
+++ Isabelle/MainHC.thy
@@ -6,41 +6,73 @@ uncurryOp :: "('a => 'b => 'c) => 'a * 'b => 'c"
 
 pairL :: "'a option => 'b => ('a * 'b) option"
 "pairL l b == case l of 
-                None => None
-              | Some a => Some (a, b)"
+    None => None
+  | Some a => Some (a, b)"
 
 pairR :: "'a => 'b option => ('a * 'b) option"
 "pairR a r == case r of 
-                None => None
-              | Some b => Some (a, b)"
+    None => None
+  | Some b => Some (a, b)"
 
-consts app :: "('a => 'b option) option => 'a option => 'b option"
-primrec
-  "app None a = None"
-  "app (Some f) x = (case x of 
-                            None => None
-                          | Some x' => f x')"
+appl1 :: "('a => 'b option) => 'a option => 'b option"
+"appl1 f s == case s of
+    None => None
+  | Some a => f a"
 
-consts apt :: "('a => 'b) option => 'a option => 'b option"
-primrec
-  "apt None a = None"
-  "apt (Some f) x = (case x of 
-                            None => None
-                          | Some x' => Some (f x'))"
+appl3 :: "('a => 'b) => 'a option => 'b option"
+"appl3 f s == case s of
+    None => None
+  | Some a => Some (f a)"
 
-consts pApp :: "('a => bool) option => 'a option => bool"
-primrec
-  "pApp None a = False"
-  "pApp (Some f) x = (case x of
-                             None => False
-                           | Some y => f y)"
 
+appl5 :: "('a => 'b option) option => 'a => 'b option"
+"appl5 s a == case s of
+    None => None
+  | Some f => f a"
+
+app :: "('a => 'b option) option => 'a option => 'b option"
+"app s t == case s of
+    None => None
+  | Some f => (case t of
+      None => None
+    | Some a => f a)"
+
+apt :: "('a => 'b) option => 'a option => 'b option"
+"apt s t == case s of
+    None => None
+  | Some f => (case t of
+      None => None
+    | Some a => Some (f a))"
+
+appl7 :: "('a => 'b) option => 'a => 'b option"
+"appl7 s a == case s of
+    None => None
+  | Some f => Some (f a)"
+
+pApp1 :: "('a => bool) option => 'a => bool"
+"pApp1 s a == case s of
+    None => False
+  | Some f => f a"
+
+pApp2 :: "('a => bool) => 'a option => bool"
+"pApp2 f s == case s of
+    None => False
+  | Some a => f a"
+
+pApp :: "('a => bool) option => 'a option => bool"
+"pApp s t == case s of
+    None => False
+  | Some f => (case t of
+      None => False
+    | Some a => f a)"
+
+defOp1 :: "'a => bool"
+"defOp1 a == True"
 
 consts defOp :: "'a option => bool"
 primrec
   "defOp (Some x) = True"
   "defOp None     = False"
-
 
 consts pair :: "'a option => 'b option => ('a * 'b) option"
 primrec
@@ -48,7 +80,6 @@ primrec
   "pair (Some x) z = (case z of
                              None => None
                            | Some y => Some (x,y))"
-
 
 lemma some_inj : "Some x = Some y ==> x = y"
 apply (auto)
