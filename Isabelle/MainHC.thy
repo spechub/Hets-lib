1bb2a0c2b6c21e18310afb81fb11061da64f186d
Christian Maeder <Christian.Maeder@dfki.de>
2006-06-08 20:20:29 +0000

    added conversion functions
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@613 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/MainHC.thy
+++ Isabelle/MainHC.thy
@@ -3,8 +3,69 @@ imports Main
 begin
 
 constdefs
+unpackOp :: "(('a => 'b) => 'c => 'd) => ('a => 'b) option => 'c => 'd option"
+"unpackOp c s a == case s of
+    None => None
+  | Some f => Some (c f a)"
+
 uncurryOp :: "('a => 'b => 'c) => 'a * 'b => 'c"
 "uncurryOp f p == f (fst p) (snd p)"
+
+curryOp :: "('a * 'b => 'c) => 'a => 'b => 'c"
+"curryOp f a b == f (a, b)"
+
+flipOp :: "('a => 'b => 'c) => 'b => 'a => 'c" 
+"flipOp f a b == f b a"
+
+option2bool :: "'a option => bool"
+"option2bool s == case s of 
+    None => False
+  | Some a => True"
+
+bool2option :: "bool => unit option"
+"bool2option b == if b then Some () else None"
+
+liftUnit2unit :: "('a => 'b) => bool => bool" 
+"liftUnit2unit f b == b"
+
+liftUnit2bool :: "(unit => bool) => bool => bool" 
+"liftUnit2bool f b == if b then f () else False"
+
+liftUnit2option :: "(unit => 'a option) => bool => 'a option" 
+"liftUnit2option f b == if b then f () else None"
+
+liftUnit :: "(unit => 'a) => bool => 'a option" 
+"liftUnit f b == if b then Some (f ()) else None"
+
+lift2unit :: "('b => 'c) => ('a option => bool)" 
+"lift2unit f == option2bool"
+
+(* pApp2 *)
+lift2bool :: "('a => bool) => 'a option => bool" 
+"lift2bool f s == case s of
+    None => False
+  | Some a => f a"
+
+(* appl1 *)
+lift2option :: "('a => 'b option) => 'a option => 'b option" 
+"lift2option f s == case s of
+    None => None
+  | Some a => f a"
+
+mapSome :: "('a => 'b) => 'a option => 'b option"
+"mapSome f s == case s of 
+    None => None 
+  | Some a => Some (f a)" 
+
+liftFst :: "(('a => 'c) => 'd => 'e) => ('a * 'b => 'c) => 'd * 'b => 'e"
+"liftFst f g p == f (flipOp (curryOp g) (snd p)) (fst p)"
+
+liftSnd :: "(('b => 'c) => 'f => 'g) => ('a * 'b => 'c) => 'a * 'f => 'g"
+"liftSnd f g p == f (curryOp g (fst p)) (snd p)"
+
+liftPair :: "(('a => 'c) => 'd => 'e) => (('b => 'e) => 'f => 'g) 
+             => ('a * 'b => 'c) => 'd * 'f => 'g"
+"liftPair f g == liftSnd g o liftFst f"
 
 pairL :: "'a option => 'b => ('a * 'b) option"
 "pairL l b == case l of 
@@ -16,6 +77,9 @@ pairR :: "'a => 'b option => ('a * 'b) option"
     None => None
   | Some b => Some (a, b)"
 
+lift :: "('a option => 'b option) => 'a => 'b option"
+"lift g a == g (Some a)"
+
 appl1 :: "('a => 'b option) => 'a option => 'b option"
 "appl1 f s == case s of
     None => None
@@ -25,7 +89,6 @@ appl3 :: "('a => 'b) => 'a option => 'b option"
 "appl3 f s == case s of
     None => None
   | Some a => Some (f a)"
-
 
 appl5 :: "('a => 'b option) option => 'a => 'b option"
 "appl5 s a == case s of
@@ -46,9 +109,6 @@ pApp2 :: "('a => bool) => 'a option => bool"
 "pApp2 f s == case s of
     None => False
   | Some a => f a"
-
-defOp1 :: "'a => bool"
-"defOp1 a == True"
 
 consts app :: "('a => 'b option) option => 'a option => 'b option"
 primrec
