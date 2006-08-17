360a37b600f6158e113929ed2df53774fe66a391
Christian Maeder <Christian.Maeder@dfki.de>
2006-08-17 13:47:32 +0000

    changed header and cleaned up
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@636 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/HsHOL.thy
+++ Isabelle/HsHOL.thy
@@ -1,5 +1,6 @@
-
-theory HsHOL = Main:
+theory HsHOL
+imports Main
+begin
 
 axclass hskTerm < type
 
@@ -13,9 +14,9 @@ hNEq :: "('a::Eq) => 'a => bool"
 instance "*" :: ("hskTerm","hskTerm") hskTerm ..
 
 instance bool :: hskTerm
-by intro_classes 
+by intro_classes
 instance int :: hskTerm
-by intro_classes 
+by intro_classes
 instance bool :: Eq
 by intro_classes
 instance list :: ("Eq") Eq
@@ -25,17 +26,7 @@ by intro_classes
 
 instance list :: ("hskTerm") hskTerm ..
 
-(*
-axioms 
-
-ax1: "hEq p p == Some true"
-
-ax2: "[| hEq q r = Some true; hEq p q = Some true|] ==> hEq p r = Some true"
-
-ax3: "hEq p q = Some true == hNEq p q = Some false"
-*)
-
-axioms 
+axioms
 
 ax1: "hEq p p"
 
@@ -44,46 +35,3 @@ ax2: "[| hEq q r; hEq p q |] ==> hEq p r"
 ax3: "hEq p q == ~ hNEq p q"
 
 end
-
-
-(*
-
-types 
- opBool = "bool option"
- opInt = "int option"
-
-constdefs 
-opAnd :: "opBool => opBool => opBool"
-"opAnd a b == case a of 
-   None => None 
-   | Some x => case b of 
-      None => None 
-      | Some y => Some (x & y)"
-
-constdefs
-opOr :: "opBool => opBool => opBool"
-"opOr a b == case a of 
-   None => case b of
-      None => None
-      | Some y => b
-   | Some x => case b of 
-      None => a
-      | Some y => Some (x | y)"
-
-constdefs
-fopbin :: "(('a::type) => ('b::type) => ('c::type)) => ('a option => 'b option => 'c option)"
-"fopbin f a b == case a of 
-   None => None
-   | Some x => case b of 
-      None => None
-      | Some y => Some (f x y)"
-
-opTrue :: opBool 
-"opTrue == Some True"
-
-opFalse :: opBool 
-"opFalse == Some False"
-
-
-end
-*)
--- Isabelle/HsHOLCF.thy
+++ Isabelle/HsHOLCF.thy
@@ -1,9 +1,10 @@
-
-theory HsHOLCF = HOLCF:
+theory HsHOLCF
+imports HOLCF
+begin
 
 axclass hskTerm < type
 
-types 
+types
 dInt = "int lift"
 
 axclass Ord < type
@@ -17,18 +18,19 @@ instance "->" :: ("hskTerm","hskTerm") hskTerm ..
 instance "*" :: ("hskTerm","hskTerm") hskTerm ..
 
 instance bool :: hskTerm
-by intro_classes 
+by intro_classes
 instance int :: hskTerm
-by intro_classes 
-instance bool :: Eq .. 
+by intro_classes
+instance bool :: Eq ..
 instance int :: Eq ..
 
-instance lift :: (Eq) Eq .. 
-instance lift :: (hskTerm) hskTerm .. 
+instance lift :: (Eq) Eq ..
+instance lift :: (hskTerm) hskTerm ..
 
-constdefs 
-fliftbin :: "(('a::type) => ('b::type) => ('c::type)) => ('a lift -> 'b lift -> 'c lift)"
-"fliftbin f == (LAM y. (LAM x. (((flift1 (%u. (flift2 (%v. f v u))))$x)$y)))"  
+constdefs
+fliftbin ::
+"(('a::type) => ('b::type) => ('c::type)) => ('a lift -> 'b lift -> 'c lift)"
+"fliftbin f == (LAM y. (LAM x. (((flift1 (%u. (flift2 (%v. f v u))))$x)$y)))"
 
 (* a type of lazy lists (from Fixrec_ex) *)
 domain 'a llist = lNil | lCons (lazy lHd ::'a) (lazy lTl ::"'a llist")
@@ -37,15 +39,18 @@ instance llist :: ("{hskTerm,pcpo}") hskTerm ..
 instance llist :: ("{Eq,pcpo}") Eq ..
 
 defs
-tr_hEq_def: "hEq == LAM (a::tr) b. if (a = UU) | (b = UU) then UU else Def (a = b)"
-dInt_hEq_def: "hEq == LAM (a::dInt) b. if (a = UU) | (b = UU) then UU else Def (a = b)"
+tr_hEq_def:
+"hEq == LAM (a::tr) b. if (a = UU) | (b = UU) then UU else Def (a = b)"
 
-axioms 
+dInt_hEq_def:
+"hEq == LAM (a::dInt) b. if (a = UU) | (b = UU) then UU else Def (a = b)"
+
+axioms
 
 ax1: "hEq $ p $ p == Def true"
 
-ax2: "[| hEq $ q $ r = Def true; hEq $ p $ q = Def true |] ==> 
-   hEq $ p $ r = Def true"
+ax2: "[| hEq $ q $ r = Def true; hEq $ p $ q = Def true |]
+      ==> hEq $ p $ r = Def true"
 
 ax3: "hEq $ p $ q = Def true == hNEq $ p $ q = Def false"
 
