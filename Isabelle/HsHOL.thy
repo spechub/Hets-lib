bb4d5b489d1971d73752d43ae8c4da0b92ec4e2d
Paolo Torrini <paolot@informatik.uni-bremen.de>
2007-11-05 11:01:20 +0000

    changes in classes
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@785 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/HsHOL.thy
+++ Isabelle/HsHOL.thy
@@ -1,24 +1,25 @@
 theory HsHOL
-imports Main
+imports "$ISABELLE_HOME/src/HOL/Complex/Complex"
 begin
 
 types
-unitT  = unit
-boolT  = bool
-intT   = int 
-integerT = int
-charT  = char
-stringT = string
 'a maybeT = "'a option"
 'a listT  = "'a list"
 
+datatype sOrdering = LT | EQ | GT
+
+axclass Bounded < type
 axclass Eq < type
+axclass Num < Eq
+axclass Ord < Eq
+axclass Enum < type
 
 instance unit :: Eq ..
 instance int :: Eq ..
 instance bool :: Eq ..
-(* instance rat :: Eq .. *)
+instance rat :: Eq .. 
 instance char :: Eq ..
+instance int :: Num ..
 
 instance list :: (Eq) Eq ..
 instance "*" :: (Eq, Eq) Eq .. 
@@ -26,23 +27,37 @@ instance "+" :: (Eq,Eq) Eq ..
 instance option :: (Eq) Eq ..
 
 consts
-hEq :: "'a => 'a => bool"
-hNEq :: "'a => 'a => bool"
+eqH :: "('a::Eq) => 'a => bool"
+neqH :: "('a::Eq) => 'a => bool"
 
 defs
-bool_hEq_def: "hEq (x::bool) y == x = y" 
-bool_hNEq_def: "hNEq (x::bool) y == ~hEq x y"
-int_hEq_def: "hEq (x::int) y == x = y" 
-int_hNEq_def: "hNEq (x::int) y == ~hEq x y"
+bool_eqH_def: "eqH (x::bool) y == x = y" 
+bool_neqH_def: "neqH (x::bool) y == ~eqH x y"
+int_eqH_def: "eqH (x::int) y == x = y" 
+int_neqH_def: "neqH (x::int) y == ~eqH x y"
 
 primrec
-"hEq [] (ys:: 'a list) = (ys = [])"
-"hEq (x#xs) (ys:: 'a list) = ((hEq x (hd ys)) & (hEq xs (tl ys)))" 
+"eqH [] (ys:: ('a::Eq) list) = (ys = [])"
+"eqH (x#xs) (ys:: ('a::Eq) list) = ((eqH x (hd ys)) & (eqH xs (tl ys)))" 
 
 defs
-list_hNEq_def: "hNEq (x::'a list) y == ~hEq x y"
+list_neqH_def: "neqH (x::('a::Eq) list) y == ~eqH x y"
+
+consts 
+addH   :: "'a::Num => 'a => 'a"
+diffH  :: "'a::Num => 'a => 'a"
+prodH   :: "'a::Num => 'a => 'a"
+negateH :: "'a::Num => 'a"
+absH    :: "'a::Num => 'a"
+signumH :: "'a::Num => 'a"
+fromIntegerH :: "int => 'a::Num"
+zeroH        :: "'a::Num"
+oneH         :: "'a::Num"
+twoH         :: "'a::Num"
 
 consts
+eqHI :: "('a::Eq) => 'a => bool"
+neqHI :: "('a::Eq) => 'a => bool"
 failDF :: "string => 'a"
 mbbind :: "'a => 'b => 'b"
 
