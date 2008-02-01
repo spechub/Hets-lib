0410916f65899b65eb7b620781156db3c3839e36
Unknown <sergey@unknown>
2008-02-01 15:45:29 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@804 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/Hoare/Theories/KleeneSyntax.thy
+++ HasCASL/Hoare/Theories/KleeneSyntax.thy
@@ -4,33 +4,54 @@ text{*Definition of monad type and the two monadic funtions
   \quak{@{text "\<guillemotright>="}} and ret *}
 
 consts   
-  star1 :: "('a \<Rightarrow> 'a T) \<Rightarrow> ('a \<Rightarrow> 'a T)" ("_\<^sup>\<star>" [100] 10)
+  uplus :: "('a \<Rightarrow> 'a T) \<Rightarrow> ('a \<Rightarrow> 'a T)" ("_^[+]" [1000] 999)
+
+notation (xsymbols) uplus ("_\<^sup>+" [1000] 999)
 
 axioms
- unf_left:   "(p\<^sup>\<star>) = (\<lambda>x. p x \<oplus> ((p\<^sup>\<star>) x \<guillemotright>= p))"
- unf_right:  "(p\<^sup>\<star>) = (\<lambda>x. p x \<oplus> (p x \<guillemotright>= (p\<^sup>\<star>)))"
- ind_left:   "\<forall>x. ((p x \<guillemotright>= q) \<preceq> (q x)) \<Longrightarrow> \<forall>x. (((p\<^sup>\<star>) x \<guillemotright>= q) \<preceq> (q x))"
- ind_right:  "\<forall>x. ((p x \<guillemotright>= q) \<preceq> (p x)) \<Longrightarrow> \<forall>x. ((p x \<guillemotright>= (q\<^sup>\<star>)) \<preceq> (p x))"
+ unf_left:   "(p^[+]) = (\<lambda>x. p x \<oplus> ((p^[+]) x \<guillemotright>= p))"
+ unf_right:  "(p^[+]) = (\<lambda>x. p x \<oplus> (p x \<guillemotright>= (p^[+])))"
+ ind_left:   "\<forall>x. ((p x \<guillemotright>= q) \<preceq> (q x)) \<Longrightarrow> \<forall>x. (((p^[+]) x \<guillemotright>= q) \<preceq> (q x))"
+ ind_right:  "\<forall>x. ((p x \<guillemotright>= q) \<preceq> (p x)) \<Longrightarrow> \<forall>x. ((p x \<guillemotright>= (q^[+])) \<preceq> (p x))"
 
 constdefs
   test :: "bool \<Rightarrow> unit T"
   "(test b) == if b then ret () else \<delta>" 
 
 syntax
-  (* "_monseq"  :: "monseq \<Rightarrow> 'a T"                  ("(do {(_)})"    [5] 100) *)
-  "_monstar"  :: "[pttrn, 'a T, monseq]\<Rightarrow> monseq"  ("((_\<leftarrow>\<^sup>\<star>(_));/ _)" [110,6,5]5)
-  (* "_monexp"  :: "['a T, monseq]\<Rightarrow> monseq"         ("((_);/ _)"     [6,5] 5)
-  "_monexp0" :: "['a T] \<Rightarrow> monseq"                ("(_)"           5) *)
+  "_monstar"  :: "[pttrn, 'a T, monseq]\<Rightarrow> monseq"  ("((_\<leftarrow>\<^sup>*(_));/ _)" [110,6,5]5)
 
 translations
-  (* input macros; replace do-notation by bind/seq *)
-  "_monseq(_monstar x p q)"    => "(_monseq q) \<oplus> ((((%x. p)\<^sup>\<star>) x) \<guillemotright>= (%x. (_monseq q)))"
-  (* Retranslation of bind/seq into do-notation *)
-  (*"_monseq(_monstar x p q)"    <= "p \<guillemotright>= (%x. q)"*)
-  (* Normalization macros *)
+  "_monseq(_monstar x p q)"    => "(_monseq q) \<oplus> ((((%x. p)^[+]) x) \<guillemotright>= (%x. (_monseq q)))"
   "_monseq(_monstar x p q)"    <= "_monseq (_monstar x p (_monseq q))"
 
-lemma unfLeft [simp]: "(do {x \<leftarrow>\<^sup>\<star> p x; q x}) = (q x \<oplus> do{x \<leftarrow>\<^sup>\<star> p x; x \<leftarrow> p x; q x})" 
-  
+lemma unfLeft: "(do {x \<leftarrow>\<^sup>* p x; q x}) = (q x \<oplus> do{x \<leftarrow>\<^sup>* p x; x \<leftarrow> p x; q x})"
+proof -
+  have "(q x \<oplus> ((p^[+]) x \<guillemotright>= q)) = (q x \<oplus> (p x \<oplus> ((p^[+]) x \<guillemotright>= p) \<guillemotright>= q))"
+    using unf_left [of p] by (rule_tac f="%z. (q x \<oplus> ((z x) \<guillemotright>= q))" in arg_cong, auto) (*, rule_tac x="x" in fun_cong, auto*)
+  thus ?thesis by (simp add: dist1)
+qed
 
+lemma unfRight: "(do {x \<leftarrow>\<^sup>* p x; q x}) = (q x \<oplus> do{x \<leftarrow> p x; x \<leftarrow>\<^sup>* p x; q x})"
+proof -
+  have "(q x \<oplus> ((p^[+]) x \<guillemotright>= q)) = (q x \<oplus> (p x \<oplus> (p x \<guillemotright>= (p^[+])) \<guillemotright>= q))"
+    using unf_right [of p] by (rule_tac f="%z. (q x \<oplus> ((z x) \<guillemotright>= q))" in arg_cong, auto)
+  thus ?thesis by (simp add: dist2)
+qed
+
+lemma indLeft:
+  assumes "\<forall>x. (do {x \<leftarrow> p x; q x})  \<preceq> q x"
+  shows   "\<forall>x. (do {x \<leftarrow>\<^sup>* p x; q x}) \<preceq> q x"
+proof-
+  have "\<forall>x. (((p^[+]) x \<guillemotright>= q) \<preceq> (q x))"
+    using assms by (rule ind_left)
+  hence "\<forall>x. (q x = (((p^[+]) x \<guillemotright>= q) \<oplus> (q x)))"
+    by (unfold "ileq_def") simp
+  hence "\<forall>x. (q x = (((p^[+]) x \<guillemotright>= q) \<oplus> (q x) \<oplus> (q x)))"    
+    by (simp add: idmp)
+  hence "\<forall>x. (q x = ((q x) \<oplus> ((p^[+]) x \<guillemotright>= q) \<oplus> (q x)))"    
+    by (unfold "comm") simp
+  thus ?thesis
+    by (unfold ileq_def) simp
+qed
 end
