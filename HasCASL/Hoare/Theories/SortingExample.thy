2ac38aea10427e0ac51bc34a9f34749771758a30
Unknown <sergey@unknown>
2008-02-07 20:12:05 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@808 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/Hoare/Theories/SortingExample.thy
+++ HasCASL/Hoare/Theories/SortingExample.thy
@@ -1,8 +1,17 @@
 theory SortExample imports KleeneSyntax begin
-  
+
+text {* This is a demonstration example tailored in order to expose 
+Kleene Monad functionality. Here we introduce two different variants
+of the BubbleSort algorithm and show their equivalence. *}   
+
 syntax
   "_do_while" :: "[bool T, pttrn, 'a T, 'a T] \<Rightarrow> 'a T"  ("dowhile {_, _, _, _}")
-  
+
+
+-- "b - continuation condition"
+-- "x - variable, providing an interface, between the neighbor steps of the loop"
+-- "p - initial computation passed to the loop trought x"
+-- "q - the loop's body"  
 translations
   "dowhile {b, x, p, q}"  => "(q \<guillemotright>= ((%x. (test b) \<guillemotright> p)^[*]))"
 
@@ -13,12 +22,16 @@ consts
   "go_home"  :: "unit T"
   "try_goto" :: "nat \<Rightarrow> bool T" 
 
-(*  "p == dowhile {ret b, b, ret b, ret True}" *)
-
 constdefs
-  "bubble_sort  == dowhile {ret b, b, dowhile {try_next, b, do {x \<leftarrow> try_swap; ret (b \<or> x)}, do{go_home; ret \<bottom>}}, ret \<top>}"
-  "bubble_sort' == dowhile {try_goto n, n, dowhile {try_next, b, do {try_swap; ret b}, ret (n+1)}, ret 0}"
-   
+  "bubble_sort  == dowhile {ret b, b, 
+                            dowhile {try_next, b, do {x \<leftarrow> try_swap; ret (b \<or> x)}, 
+                                     do{go_home; ret \<bottom>}}, ret \<top>}"
+  "bubble_sort' == dowhile {try_goto n, n, 
+                            dowhile {try_next, b, do {try_swap; ret b}, 
+                                     ret (n+1)}, ret 0}"
+
+
+-- "proof of this theorem will call for some lemmas about @{const dowhile}"   
 theorem "do{bubble_sort; ret ()} = do {bubble_sort'; ret ()}"
   sorry
 			      
