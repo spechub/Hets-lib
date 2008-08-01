a8a366049a1dc49e9c203c06585c1aab08bab556
Unknown <sergey@unknown>
2008-08-01 08:40:24 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@890 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/Hoare/Theories/StackReverse.thy
+++ HasCASL/Hoare/Theories/StackReverse.thy
@@ -30,6 +30,15 @@ constdefs
   "rev == do {p \<leftarrow> star {p \<leftarrow> ret empty; x \<leftarrow> pop; ret (do {push x; p})}; p}"
   rev_susp :: "unit T T \<Rightarrow> unit T T T"
   "(rev_susp l) == star {p \<leftarrow> ret l; ret (do{x \<leftarrow> pop; t \<leftarrow> p; ret (do {t; push x})})}"
+  rev_dup_susp :: "unit T T \<Rightarrow> unit T T \<Rightarrow> (unit T T \<times> unit T T) T"
+  "(rev_dup_susp p q) == star {(p, q) \<leftarrow> ret (p, q); ret (do{x \<leftarrow> pop; t \<leftarrow> p; ret (do {t; push x})},
+                                                          do{x \<leftarrow> pop; t \<leftarrow> q; ret (do {t; push x})})}"
+  id_susp :: "unit T T \<Rightarrow> unit T T T"
+  "(id_susp l) == star {p \<leftarrow> ret l; ret (do{x \<leftarrow> pop; t \<leftarrow> p; ret (do {push x; t})})}"
+  length_inv :: "unit T \<Rightarrow> bool"
+  "(length_inv p) == (do{q \<leftarrow> id_susp (ret (ret ())); z \<leftarrow> q; z; p; z \<leftarrow> q; z} = p)"
+  length_mut :: "unit T \<Rightarrow> bool"
+  "(length_mut p) == (do{q \<leftarrow> id_susp (ret (ret ())); z \<leftarrow> q; z; p; z \<leftarrow> q; z} = \<delta>)"
 
 text{*
   The most essential stack axioms reflect the duality of  @{text"pop"},
@@ -68,32 +77,53 @@ text{* Following lemma is a variant of the double reverse property.
 
 lemma rev_rev_fin : "do{p \<leftarrow> star {p \<leftarrow> ret (ret (ret ())); ret (do{x \<leftarrow> pop; t \<leftarrow> p; ret (do {t; push x})})}; 
                         z \<leftarrow> p; z; z \<leftarrow> p; z} \<preceq> ret ()"
-apply (rule ileq_assoc [of _ "do{q \<leftarrow> star {p \<leftarrow> ret (ret ()); x \<leftarrow> pop; ret (do {push x; p})}; q}"])
-apply (insert rev_lemma_inst)
-apply (subst (asm) unf_left) back
-apply (subst (asm) comm)
-apply simp
-apply (rule ileq_plusE)
-apply assumption
-apply (subst fstUnitLaw [THEN sym, of id "ret ()", unfolded id_def]) back
-apply (rule indLeft [THEN allE])
-prefer 2
-apply assumption
-by simp
+  apply (rule ileq_assoc [of _ "do{q \<leftarrow> star {p \<leftarrow> ret (ret ()); x \<leftarrow> pop; ret (do {push x; p})}; q}"])
+  apply (insert rev_lemma_inst)
+  apply (subst (asm) unf_left) back
+  apply (subst (asm) comm)
+  apply simp
+  apply (rule ileq_plusE)
+  apply assumption
+  apply (subst fstUnitLaw [THEN sym, of id "ret ()", unfolded id_def]) back
+  apply (rule indLeft [THEN allE])
+  prefer 2
+  apply assumption
+  by simp
 
-lemma rev_susp_dup :  "do {p \<leftarrow> rev_susp (ret is_empty); q \<leftarrow> rev_susp (ret is_empty); z \<leftarrow> p; z; z \<leftarrow> q; z} = 
-                      (do {p \<leftarrow> rev_susp (ret is_empty); q \<leftarrow> rev_susp (p); z \<leftarrow> p; z; z \<leftarrow> q; z} \<oplus> 
-                       do {q \<leftarrow> rev_susp (ret is_empty); p \<leftarrow> rev_susp (p); z \<leftarrow> p; z; z \<leftarrow> q; z})"
+lemma rev_dup_lemma : "do{(p, q) \<leftarrow> rev_dup_susp p q;
+                      t \<leftarrow> star {p \<leftarrow> ret (ret ()); x \<leftarrow> pop; ret (do {push x; p})}; z \<leftarrow> p; z; z \<leftarrow> q; z; t} \<preceq>
+                   do{t \<leftarrow> star {p \<leftarrow> ret (ret ()); x \<leftarrow> pop; ret (do {push x; p})}; z \<leftarrow> p; z; z \<leftarrow> q; z; t}"
+  apply (unfold rev_dup_susp_def)
+  apply (subst fstUnitLaw)
+  apply (rule_tac x = "(p, q)" and q1 = "\<lambda>(x,y).(?q2 x y)" in ind_left [THEN allE])
+  prefer 2
+  apply simp
+  apply (subst unf_right) back
+  apply simp
+  apply (subst comm)
+  apply (rule allI)+
+  by (rule ileq_plusMon)
 
-lemma "do {t \<leftarrow> ret(rev_susp (ret (ret (ret ())))); p \<leftarrow> rev_susp (p); q \<leftarrow> ret (do {t; p}); z \<leftarrow> p; z; z \<leftarrow> q; z} \<preceq> 
-       do {q \<leftarrow> rev_susp (p); z \<leftarrow> p; z; z \<leftarrow> q; z}"
+lemma cut_dup_lemma :  "do {p \<leftarrow> rev_susp p; q \<leftarrow> rev_susp q; z \<leftarrow> p; z; z \<leftarrow> q; z} = 
+                       (do {p \<leftarrow> rev_susp p; (p, q) \<leftarrow> rev_dup_susp p q; z \<leftarrow> p; z; z \<leftarrow> q; z} \<oplus> 
+                        do {q \<leftarrow> rev_susp q; (p, q) \<leftarrow> rev_dup_susp p q; z \<leftarrow> p; z; z \<leftarrow> q; z})"
+sorry
+
+-- "should follow from rev_dup_lemma"
+lemma cut_dup_lema1 : "do {p \<leftarrow> rev_susp (ret is_empty); (p, q) \<leftarrow> rev_dup_susp p (ret is_empty); z \<leftarrow> p; z; z \<leftarrow> q; z} = 
+                       do {(p, q) \<leftarrow> rev_dup_susp (ret is_empty) (ret is_empty); z \<leftarrow> p; z; z \<leftarrow> q; z}"
+sorry
+
+-- "should follow from rev_dup_lemma"
+lemma cut_dup_lema2 : "do {q \<leftarrow> rev_susp (ret is_empty); (p, q) \<leftarrow> rev_dup_susp (ret is_empty) q; z \<leftarrow> p; z; z \<leftarrow> q; z} =
+                       do {(p, q) \<leftarrow> rev_dup_susp (ret is_empty) (ret is_empty); z \<leftarrow> p; z; z \<leftarrow> q; z}"
+sorry
 
 
-(*
-text{* Double reverse lemma, as it was originally thougt. It is expected 
-  to follow from  @{text"rev_rev_fin"}. *}
+text{* Double reverse lemma, as it was originally thougt. *}
+-- "should follow from cut_dup_lemma"
 lemma rev_rev : "do {rev; rev} \<preceq> (ret ())"
   sorry
-*)
+
 
 end
