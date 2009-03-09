9bf8067d2ce5504e217cf2eb3a7cc2fdb04b5e7d
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2009-03-09 10:03:03 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1128 9acf5e20-c632-0410-b30d-e55f0e25a569

---
+++ HasCASL/Functions_option_manual.thy
@@ -0,0 +1,47 @@
+theory Functions_Functions
+imports "$HETS_LIB/Isabelle/MainHC"
+uses "$HETS_LIB/Isabelle/prelude"
+begin
+
+ML "Header.initialize
+    [\"o_def\", \"id_def\", \"o_assoc\", \"id_neut\", \"inj\",
+     \"surj\"]"
+
+consts
+X__o__X :: "('b => 'c partial) * ('a => 'b partial) => 'a => 'c partial"
+X_id :: "'a => 'a partial" ("id''/'(_')" [3] 999)
+
+
+axioms
+o_def [rule_format] :
+"ALL f.
+ ALL g.
+ X__o__X (f, g) =
+ (% x. restrictOp (makeTotal (f (makeTotal (g x)))) ( (defOp (g x)) & defOp (f x)))"
+
+id_def [rule_format] : "X_id = makePartial o (% x. x)"
+
+theorem o_assoc :
+"ALL f.
+ ALL g.
+ ALL h. X__o__X (f, X__o__X (g, h)) = X__o__X (X__o__X (f, g), h)"
+done
+
+theorem id_neut : "ALL f'. X__o__X (f', X_id) = f'"
+done
+
+
+theorem inj  :
+"ALL f'.
+ X__o__X (f', f') = X_id -->
+ (ALL x. ALL y. f' x =s= f' y --> x = y)"
+done
+
+
+theorem surj  :
+"ALL f'.
+ X__o__X (f', f') = X_id --> (ALL x. EX y. f' y = makePartial x)"
+done
+
+
+end
