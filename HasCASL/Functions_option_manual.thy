392f41ebfd6ba748939054191cd5832ed6e40055
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2009-03-09 17:00:54 +0000

    assoc + lemmata
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1131 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/Functions_option_manual.thy
+++ HasCASL/Functions_option_manual.thy
@@ -17,14 +17,40 @@ o_def [rule_format] :
 "ALL f.
  ALL g.
  X__o__X (f, g) =
- (% x. restrictOp ( (makeTotal  o f) ((makeTotal o g) x)) ( (defOp (g x)) & defOp (f ((makeTotal o g) x))))"
+ (% x. restrictOp ( f ((makeTotal o g) x)) (defOp (g x)))"
 
 id_def [rule_format] : "X_id = makePartial o (% x. x)"
 
+theorem restrict1 : "restrictOp (restrictOp a (defOp b)) c = restrictOp (restrictOp a c) (defOp (restrictOp b c))"
+apply (case_tac c)
+apply (simp add: restrictOp_def)
+apply (simp add: restrictOp_def)
+done
+
+theorem restrict_trivial : "restrictOp t True = t"
+apply (simp add: restrictOp_def)
+done
+
+theorem restrict_assoc : "restrictOp a (defOp (restrictOp b c)) = restrictOp (restrictOp a (defOp b)) c"
+apply (case_tac c)
+apply (simp add: restrictOp_def)
+apply (simp add: restrictOp_def noneOp_def defOp.simps)
+done
+
+theorem restrict_out : "restrictOp (t b) b = restrictOp (t True) b"
+apply (case_tac "b")
+apply (simp add: restrictOp_def)
+apply (simp add: restrictOp_def)
+done
+
 theorem o_assoc :
-"ALL f.
- ALL g.
- ALL h. X__o__X (f, X__o__X (g, h)) = X__o__X (X__o__X (f, g), h)"
+"X__o__X (f, X__o__X (g, h)) = X__o__X (X__o__X (f, g), h)"
+apply (rule ext)
+apply (simp only: o_def comp_def)
+apply (subst restrict_assoc)
+apply (subst restrict_out [of _ "defOp (h x)"]) back 
+apply (subst restrictOp_def)
+apply (simp)
 done
 
 theorem id_neut : "ALL f'. X__o__X (f', X_id) = f'"
