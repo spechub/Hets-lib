3c34ac61b503d078aaf1f2b466ec39d17868ec70
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2009-07-06 10:52:28 +0000

    removed useless old spec, changed colon to :: in MainHCPairs and changed a comment
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1252 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/MainHCPairs.thy
+++ Isabelle/MainHCPairs.thy
@@ -121,65 +121,66 @@ axioms
 
 -- "necessary when omitting the quantifiers in the axioms below"
 subtype_constant [rule_format] :
-"subt (x\<Colon>'a) (y\<Colon>'b) ==> (!!(u::'a) v::'b. subt u v)"
+"subt (x::'a) (y::'b) ==> (!!(u::'a) v::'b. subt u v)"
 
 subtype_reflexive [rule_format] :
-"subt (x\<Colon>'a) (y\<Colon>'a)"
+"subt (x::'a) (y::'a)"
 
 subtype_transitive [rule_format] :
-"[| subt (x\<Colon>'a) (y\<Colon>'b); subt (z\<Colon>'b) (t\<Colon>'c) |]  ==> subt (u\<Colon>'a) (v\<Colon>'c)"
+"[| subt (x::'a) (y::'b); subt (z::'b) (t::'c) |]  ==> subt (u::'a) (v::'c)"
 
 -- "is used for derivation of subtypes:"
 subtype_subsumption [rule_format] :
-"[| subt (x\<Colon>'a) (y\<Colon>'c); subt (z\<Colon>'b) (t\<Colon>'c);
-  (!!(z'::'c). defOp(gn_proj(z')\<Colon>'a partial) ==> defOp(gn_proj(z')\<Colon>'b partial)) |]
-  ==> (subt (u\<Colon>'a) (v\<Colon>'b))"
+"[| subt (x::'a) (y::'c); subt (z::'b) (t::'c);
+  (!!(z'::'c). defOp(gn_proj(z')::'a partial) ==> defOp(gn_proj(z')::'b partial)) |]
+  ==> (subt (u::'a) (v::'b))"
 
 
 -- "INJECTION PROJECTION RULES"
 
 -- "is used to derive defining predicate P for a subtype A, P(gn_inj(x::A))"
 gn_proj_def [rule_format] :
-"subt (x\<Colon>'a) (y\<Colon>'b) ==> defOp(gn_proj(gn_inj(x)\<Colon>'b)\<Colon>'a partial)"
+"subt (x::'a) (y::'b) ==> defOp(gn_proj(gn_inj(x)::'b)::'a partial)"
 
 gn_proj_inj [rule_format] :
-"subt (x\<Colon>'a) (y\<Colon>'b) ==> (!!(z::'a). makeTotal(gn_proj(gn_inj(z)\<Colon>'b)) = z)"
+"subt (x::'a) (y::'b) ==> (!!(z::'a). makeTotal(gn_proj(gn_inj(z)::'b)) = z)"
 
 gn_inj_proj [rule_format] :
-"subt (x\<Colon>'a) (y\<Colon>'b) ==> (!!(z::'b). defOp(gn_proj(z)\<Colon>'a partial) ==>
-  gn_inj(makeTotal(gn_proj(z)\<Colon>'a partial)) = z)"
+"subt (x::'a) (y::'b) ==> (!!(z::'b). defOp(gn_proj(z)::'a partial) ==>
+  gn_inj(makeTotal(gn_proj(z)::'a partial)) = z)"
 
 gn_inj_diagram [rule_format] :
-"[| subt (x\<Colon>'a) (y\<Colon>'b); subt (t\<Colon>'b) (z\<Colon>'c) |]
-  ==> (!!(x'::'a). (gn_inj(x')\<Colon>'c) = gn_inj(gn_inj(x')\<Colon>'b))"
+"[| subt (x::'a) (y::'b); subt (t::'b) (z::'c) |]
+  ==> (!!(x'::'a). (gn_inj(x')::'c) = gn_inj(gn_inj(x')::'b))"
 
+--   "subt (u::'a) (v::'b) ==> inj (gn_inj::'a => 'b)"
 
 lemma gn_inj_injective :
-  "subt (u\<Colon>'a) (v\<Colon>'b) ==> inj (gn_inj\<Colon>'a => 'b)"
+  "subt (u :: 'a) (v::'b) ==> inj (gn_inj::'a => 'b)"
   proof (rule injI)
     fix x::'a
     fix y::'a
-    assume hyp1: "subt (u\<Colon>'a) (v\<Colon>'b)"
-      and hyp2: "(gn_inj x\<Colon>'b) = gn_inj y"
+    assume hyp1: "subt (u::'a) (v::'b)"
+      and hyp2: "(gn_inj x::'b) = gn_inj y"
 
     from hyp1 subtype_constant 
-    have fact: "!!(z::'a). makeTotal(gn_proj(gn_inj(z)\<Colon>'b)) = z"
+    have fact: "!!(z::'a). makeTotal(gn_proj(gn_inj(z)::'b)) = z"
       by (rule_tac gn_proj_inj, blast)
 
-    hence "x = makeTotal(gn_proj(gn_inj(x)\<Colon>'b))" ..
-    also from hyp2 have "\<dots> = makeTotal(gn_proj(gn_inj(y)\<Colon>'b))" by simp
-    also from fact have "\<dots> = y" by simp
+    hence "x = makeTotal(gn_proj(gn_inj(x)::'b))" ..
+    also from hyp2 have "... = makeTotal(gn_proj(gn_inj(y)::'b))" by simp
+    also from fact have "... = y" by simp
     finally show "x = y" by simp
   qed
 
 lemma gn_inj_identity :
-"!!x::'a. (gn_inj(x)\<Colon>'a) = x"
+"!!x::'a. (gn_inj(x)::'a) = x"
 proof-
   fix x::'a
-  have fact: "(gn_inj(x)\<Colon>'a) = gn_inj(gn_inj(x)\<Colon>'a)"
+  have fact: "(gn_inj(x)::'a) = gn_inj(gn_inj(x)::'a)"
     by (simp_all add: gn_inj_diagram subtype_reflexive)
-  thus "(gn_inj(x)\<Colon>'a) = x"
-    by (subst injD [of "(gn_inj\<Colon>'a=>'a)" "(gn_inj(x)\<Colon>'a)" x],
+  thus "(gn_inj(x)::'a) = x"
+    by (subst injD [of "(gn_inj::'a=>'a)" "(gn_inj(x)::'a)" x],
       simp_all only: gn_inj_injective subtype_reflexive fact [symmetric])
 qed
 
