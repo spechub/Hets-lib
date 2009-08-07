afa709e07fce7f6e8f5e61972b0c8756f03f8b1a
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2009-08-07 16:41:52 +0000

    added a caption to a definition and cleaned the syntax for type annos
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1271 9acf5e20-c632-0410-b30d-e55f0e25a569

--- Isabelle/MainHCPairs.thy
+++ Isabelle/MainHCPairs.thy
@@ -121,67 +121,68 @@ axioms
 
 -- "necessary when omitting the quantifiers in the axioms below"
 subtype_constant [rule_format] :
-"subt (x::'a) (y::'b) ==> (!!(u::'a) v::'b. subt u v)"
+"subt (x:: 'a) (y:: 'b) ==> (!!(u:: 'a) v:: 'b. subt u v)"
 
 subtype_reflexive [rule_format] :
-"subt (x::'a) (y::'a)"
+"subt (x:: 'a) (y:: 'a)"
 
 subtype_transitive [rule_format] :
-"[| subt (x::'a) (y::'b); subt (z::'b) (t::'c) |]  ==> subt (u::'a) (v::'c)"
+"[| subt (x:: 'a) (y:: 'b); subt (z:: 'b) (t:: 'c) |]  ==> subt (u:: 'a) (v:: 'c)"
 
 -- "is used for derivation of subtypes:"
 subtype_subsumption [rule_format] :
-"[| subt (x::'a) (y::'c); subt (z::'b) (t::'c);
-  (!!(z'::'c). defOp(gn_proj(z')::'a partial) ==> defOp(gn_proj(z')::'b partial)) |]
-  ==> (subt (u::'a) (v::'b))"
+"[| subt (x:: 'a) (y:: 'c); subt (z:: 'b) (t:: 'c);
+  (!!(z':: 'c). defOp(gn_proj(z'):: 'a partial) ==> defOp(gn_proj(z'):: 'b partial)) |]
+  ==> (subt (u:: 'a) (v:: 'b))"
 
 
 -- "INJECTION PROJECTION RULES"
 
 -- "is used to derive defining predicate P for a subtype A, P(gn_inj(x::A))"
 gn_proj_def [rule_format] :
-"subt (x::'a) (y::'b) ==> defOp(gn_proj(gn_inj(x)::'b)::'a partial)"
+"subt (x:: 'a) (y:: 'b) ==> defOp(gn_proj(gn_inj(x):: 'b):: 'a partial)"
 
 gn_proj_inj [rule_format] :
-"subt (x::'a) (y::'b) ==> (!!(z::'a). makeTotal(gn_proj(gn_inj(z)::'b)) = z)"
+"subt (x:: 'a) (y:: 'b) ==> (!!(z:: 'a). makeTotal(gn_proj(gn_inj(z):: 'b)) = z)"
 
 gn_inj_proj [rule_format] :
-"subt (x::'a) (y::'b) ==> (!!(z::'b). defOp(gn_proj(z)::'a partial) ==>
-  gn_inj(makeTotal(gn_proj(z)::'a partial)) = z)"
+"subt (x:: 'a) (y:: 'b) ==> (!!(z:: 'b). defOp(gn_proj(z):: 'a partial) ==>
+  gn_inj(makeTotal(gn_proj(z):: 'a partial)) = z)"
 
 gn_inj_diagram [rule_format] :
-"[| subt (x::'a) (y::'b); subt (t::'b) (z::'c) |]
-  ==> (!!(x'::'a). (gn_inj(x')::'c) = gn_inj(gn_inj(x')::'b))"
+"[| subt (x:: 'a) (y:: 'b); subt (t:: 'b) (z:: 'c) |]
+  ==> (!!(x':: 'a). (gn_inj(x'):: 'c) = gn_inj(gn_inj(x'):: 'b))"
 
---   "subt (u::'a) (v::'b) ==> inj (gn_inj::'a => 'b)"
+--   "subt (u:: 'a) (v:: 'b) ==> inj (gn_inj:: 'a => 'b)"
 
 lemma gn_inj_injective :
-  "subt (u :: 'a) (v::'b) ==> inj (gn_inj::'a => 'b)"
+  "subt (u :: 'a) (v:: 'b) ==> inj (gn_inj:: 'a => 'b)"
   proof (rule injI)
-    fix x::'a
-    fix y::'a
-    assume hyp1: "subt (u::'a) (v::'b)"
-      and hyp2: "(gn_inj x::'b) = gn_inj y"
+    fix x:: 'a
+    fix y:: 'a
+    assume hyp1: "subt (u:: 'a) (v:: 'b)"
+      and hyp2: "(gn_inj x:: 'b) = gn_inj y"
 
     from hyp1 subtype_constant 
-    have fact: "!!(z::'a). makeTotal(gn_proj(gn_inj(z)::'b)) = z"
+    have fact: "!!(z:: 'a). makeTotal(gn_proj(gn_inj(z):: 'b)) = z"
       by (rule_tac gn_proj_inj, blast)
 
-    hence "x = makeTotal(gn_proj(gn_inj(x)::'b))" ..
-    also from hyp2 have "... = makeTotal(gn_proj(gn_inj(y)::'b))" by simp
+    hence "x = makeTotal(gn_proj(gn_inj(x):: 'b))" ..
+    also from hyp2 have "... = makeTotal(gn_proj(gn_inj(y):: 'b))" by simp
     also from fact have "... = y" by simp
     finally show "x = y" by simp
   qed
 
 lemma gn_inj_identity :
-"!!x::'a. (gn_inj(x)::'a) = x"
+"!!x:: 'a. (gn_inj(x):: 'a) = x"
 proof-
-  fix x::'a
-  have fact: "(gn_inj(x)::'a) = gn_inj(gn_inj(x)::'a)"
+  fix x:: 'a
+  have fact: "(gn_inj(x):: 'a) = gn_inj(gn_inj(x):: 'a)"
     by (simp_all add: gn_inj_diagram subtype_reflexive)
-  thus "(gn_inj(x)::'a) = x"
-    by (subst injD [of "(gn_inj::'a=>'a)" "(gn_inj(x)::'a)" x],
+  thus "(gn_inj(x):: 'a) = x"
+    by (subst injD [of "(gn_inj:: 'a=>'a)" "(gn_inj(x):: 'a)" x],
       simp_all only: gn_inj_injective subtype_reflexive fact [symmetric])
 qed
 
 end
+
