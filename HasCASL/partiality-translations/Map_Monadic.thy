1b2b51aeceb527c3825087d8b9d5e26e09f25f3f
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2009-10-07 13:23:19 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1322 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/partiality-translations/Map_Monadic.thy
+++ HasCASL/partiality-translations/Map_Monadic.thy
@@ -6,7 +6,6 @@ begin
 ML "Header.initialize
     [\"Ax1\", \"Ax2\", \"Ax3\", \"Ax4\", \"Ax1_5\", \"Ax2_6\"]"
 
-
 consts
 X_filter :: "'a list => ('a => bool) => 'a list"
 X_map :: "('a => 'b partial) => 'a list => 'b list partial"
@@ -36,6 +35,10 @@ declare Ax4 [simp]
 
 theorem Ax1_5 :
 "ALL (P :: 'a => bool).
+ ALL (f :: 'a => 'a partial).
+ ALL (l :: 'a list).
+ defOp (X_map f l) --> defOp (X_map f (X_filter l P))"
+"ALL (P :: 'a => bool).
  ALL (f :: 'a => 'b partial).
  ALL (l :: 'a list).
  defOp (X_map f l) --> defOp (X_map f (X_filter l P))"
@@ -50,19 +53,18 @@ sorry
 ML "Header.record \"Ax1_5\""
 
 theorem Ax2_6 :
-"ALL (Q :: 'b => bool).
- ALL (f :: 'a => 'b partial).
+"ALL (Q :: 'a => bool).
+ ALL (f :: 'a => 'a partial).
  ALL (l :: 'a list).
  defOp (X_map f l) -->
- unpack2partial id (mapPartial X_filter (X_map f l)) Q =
+ X_map f (X_filter l Q) =
  unpack2partial id (mapPartial X_filter (X_map f l)) Q"
 apply (rule allI)
 apply (rule allI)
 apply (rule allI)
 apply (induct_tac l)
 apply (simp add: Ax1 Ax2 Ax3 Ax4)
-apply (simp add: Ax1 Ax2 Ax3 Ax4)
-done
+sorry
 
 ML "Header.record \"Ax2_6\""
 
