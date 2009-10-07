c3819d9a06c676d952b74c2c41286cf384f979ca
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2009-10-07 13:17:59 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1321 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/partiality-translations/Map_Monadic.thy
+++ HasCASL/partiality-translations/Map_Monadic.thy
@@ -1,57 +1,69 @@
 theory Map_Map_E2
-imports "$HETS_ISABELLE_LIB/MainHC"
+imports "$HETS_ISABELLE_LIB/MainHC" "List" "$HETS_LIB/Isabelle/RestrictOpProps"
 uses "$HETS_ISABELLE_LIB/prelude"
 begin
 
 ML "Header.initialize
-    [\"Ax1\", \"Ax2\", \"Ax3\", \"Ax1_4\", \"Ax2_5\"]"
+    [\"Ax1\", \"Ax2\", \"Ax3\", \"Ax4\", \"Ax1_5\", \"Ax2_6\"]"
 
-typedecl ('a1) List
 
 consts
-X_filter :: "'a List => ('a => bool) => 'a List"
-X_map :: "('a => 'b partial) => 'a List => 'b List partial"
-cons :: "'a => 'a List => 'a List"
-nil :: "'a List"
+X_filter :: "'a list => ('a => bool) => 'a list"
+X_map :: "('a => 'b partial) => 'a list => 'b list partial"
 
 axioms
 Ax1 [rule_format] :
-"ALL (f :: 'a => 'b partial). X_map f nil = makePartial nil"
+"ALL (f :: 'a => 'b partial). X_map f [] = makePartial []"
 
 Ax2 [rule_format] :
 "ALL (f :: 'a => 'b partial).
- ALL (l :: 'a List).
+ ALL (l :: 'a list).
  ALL (x :: 'a).
- X_map f (cons x l) =
- unpackPartial mapPartial (mapPartial cons (f x)) (X_map f l)"
+ X_map f (Cons x l) =
+ unpackPartial mapPartial (mapPartial Cons (f x)) (X_map f l)"
 
 Ax3 [rule_format] :
 "ALL (P :: 'a => bool).
- ALL (l :: 'a List).
+ ALL (l :: 'a list).
  ALL (x :: 'a).
- X_filter (cons x l) P =
- (if P x then cons x (X_filter l P) else X_filter l P)"
+ X_filter (Cons x l) P =
+ (if P x then Cons x (X_filter l P) else X_filter l P)"
+
+Ax4 [rule_format] : "ALL (P :: 'a => bool). X_filter [] P = []"
 
 declare Ax1 [simp]
+declare Ax4 [simp]
 
-theorem Ax1_4 :
+theorem Ax1_5 :
 "ALL (P :: 'a => bool).
  ALL (f :: 'a => 'b partial).
- ALL (l :: 'a List).
+ ALL (l :: 'a list).
  defOp (X_map f l) --> defOp (X_map f (X_filter l P))"
-by (auto)
+apply (rule allI)
+apply (rule allI)
+apply (rule allI)
+apply (induct_tac l)
+apply (simp add: Ax1 Ax2 Ax3 Ax4)
+apply (simp add: Ax1 Ax2 Ax3 Ax4)
+sorry
 
-ML "Header.record \"Ax1_4\""
+ML "Header.record \"Ax1_5\""
 
-theorem Ax2_5 :
+theorem Ax2_6 :
 "ALL (Q :: 'b => bool).
  ALL (f :: 'a => 'b partial).
- ALL (l :: 'a List).
+ ALL (l :: 'a list).
  defOp (X_map f l) -->
  unpack2partial id (mapPartial X_filter (X_map f l)) Q =
  unpack2partial id (mapPartial X_filter (X_map f l)) Q"
-by (auto)
+apply (rule allI)
+apply (rule allI)
+apply (rule allI)
+apply (induct_tac l)
+apply (simp add: Ax1 Ax2 Ax3 Ax4)
+apply (simp add: Ax1 Ax2 Ax3 Ax4)
+done
 
-ML "Header.record \"Ax2_5\""
+ML "Header.record \"Ax2_6\""
 
 end
