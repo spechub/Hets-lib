ed847ba08fa550439252a35becf92d78845c5b35
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2009-10-08 13:03:08 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1335 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/partiality-translations/PartMinus_Normalizing.thy
+++ HasCASL/partiality-translations/PartMinus_Normalizing.thy
@@ -7,29 +7,21 @@ ML "Header.initialize
     [\"ga_select_pre\", \"Ax3\", \"Ax4\", \"Ax5\", \"Ax6\", \"Ax1\",
      \"Ax2\", \"Ax3_8\", \"Ax4_7\"]"
 
-datatype X_Nat = X0 ("0''") | X_suc "X_Nat" ("suc/'(_')" [3] 999)
 
 consts
-X__XGtXEq__X :: "X_Nat => X_Nat => bool" ("(_/ >=''/ _)" [44,44] 42)
-X__XMinusXQuest__X :: "X_Nat => X_Nat => X_Nat partial" ("(_/ -?/ _)" [54,54] 52)
-X__XPlus__X :: "X_Nat => X_Nat => X_Nat" ("(_/ +''/ _)" [54,54] 52)
-X_pre :: "X_Nat => X_Nat partial" ("pre/'(_')" [3] 999)
+X__XMinusXQuest__X :: "nat => nat => nat partial" ("(_/ -?/ _)" [54,54] 52)
 
 axioms
-ga_select_pre [rule_format] :
-"ALL (x_1 :: X_Nat). pre(suc(x_1)) = makePartial x_1"
-
-Ax3 [rule_format] : "0' -? 0' = makePartial 0'"
+Ax3 [rule_format] : "0 -? 0 = makePartial 0"
 
 Ax4 [rule_format] :
-"ALL (x :: X_Nat). suc(x) -? 0' = makePartial (suc(x))"
+"ALL (x :: nat). suc(x) -? 0 = makePartial (suc(x))"
 
-Ax5 [rule_format] : "ALL (x :: X_Nat). ~ defOp (0' -? suc(x))"
+Ax5 [rule_format] : "ALL (x :: nat). ~ defOp (0 -? suc(x))"
 
 Ax6 [rule_format] :
-"ALL (x :: X_Nat). ALL (y :: X_Nat). suc(x) -? suc(y) = x -? y"
+"ALL (x :: nat). ALL (y :: nat). suc(x) -? suc(y) = x -? y"
 
-declare ga_select_pre [simp]
 declare Ax3 [simp]
 declare Ax4 [simp]
 declare Ax5 [simp]
@@ -57,34 +49,44 @@ done
 ML "Header.record \"Ax2\""
 
 theorem Ax3_8 :
-"ALL (a :: X_Nat).
- ALL (b :: X_Nat).
- ALL (c :: X_Nat).
+"ALL (a :: nat).
+ ALL (b :: nat).
+ ALL (c :: nat).
  defOp
  (restrictOp (a -? makeTotal (b -? makeTotal (c -? a)))
   (defOp (b -? makeTotal (c -? a)) & defOp (c -? a))) -->
  defOp (c -? b)"
 apply (rule allI)+
 apply (rule impI)+
-apply (frule Ax2)
-apply (frule Ax1)
-apply (auto)
-apply (simp add: Ax1 Ax2)
-by (auto)
+sorry
 
 ML "Header.record \"Ax3_8\""
 
+lemma defpartminus1 [simp]: "defOp(c -? 0)"
+apply (induct_tac c)
+apply (simp add: Ax3)
+apply (auto)
+done
+
+
+lemma defpartminus: "defOp(c -? b) = (c>=b)"
+apply (induct_tac b)
+apply (simp)
+apply (auto)
+sorry
+
 theorem Ax4_7 :
-"ALL (a :: X_Nat).
- ALL (b :: X_Nat).
- ALL (c :: X_Nat).
+"ALL (a :: nat).
+ ALL (b :: nat).
+ ALL (c :: nat).
  defOp
  (restrictOp (a -? makeTotal (b -? makeTotal (c -? a)))
   (defOp (b -? makeTotal (c -? a)) & defOp (c -? a))) -->
  restrictOp (a -? makeTotal (b -? makeTotal (c -? a)))
  (defOp (b -? makeTotal (c -? a)) & defOp (c -? a)) =
  c -? b"
-by (auto)
+apply (rule allI)+
+sorry
 
 ML "Header.record \"Ax4_7\""
 
