42cb54f5287ca506fe1f44d1d2c8ccba4d5cfeeb
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2009-10-08 12:31:09 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1333 9acf5e20-c632-0410-b30d-e55f0e25a569

--- HasCASL/partiality-translations/PartialityTest_Monadic.thy
+++ HasCASL/partiality-translations/PartialityTest_Monadic.thy
@@ -1,5 +1,5 @@
 theory PartialityTest_Test_E1
-imports "$HETS_ISABELLE_LIB/MainHC"
+imports "$HETS_ISABELLE_LIB/MainHC" "$HETS_LIB/Isabelle/RestrictOpProps"
 uses "$HETS_ISABELLE_LIB/prelude"
 begin
 
@@ -19,6 +19,18 @@ Ax1 [rule_format] :
  lift2partial (X_f x) (f(y, z)) =
  lift2partial (flip X_f z) (f(x, y))"
 
+lemma assoc_dom : "defOp (f(x, y)) & defOp (f(y, z)) ==> 
+  (defOp (f(x, makeTotal (f(y, z)))) & defOp (f(y, z))) = 
+  (defOp (f(makeTotal (f(x, y)), z)) & defOp (f(x, y)))"
+apply (frule Ax1)
+sorry
+
+lemma assoc_eq : "defOp (f(x, y)) & defOp (f(y, z)) ==> 
+  f(x, makeTotal (f(y, z))) = f(makeTotal (f(x, y)), z)"
+apply (frule Ax1)
+apply (auto)
+sorry
+
 theorem Ax1_2 :
 "ALL (w :: a).
  ALL (x :: a).
@@ -28,8 +40,8 @@ theorem Ax1_2 :
  defOp (lift2partial (X_f y) (f(z, w))) -->
  lift2partial (flip X_f w) (lift2partial (X_f x) (f(y, z))) =
  lift2partial (X_f x) (lift2partial (X_f y) (f(z, w)))"
-apply (clarify)
-apply (simp add: Ax1)
+apply (auto)
+apply (simp add: assoc_eq assoc_dom)
 sorry
 
 ML "Header.record \"Ax1_2\""
