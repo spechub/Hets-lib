5921921554c65220cc95897e721702d9db042721
Soeren D. Schulze <soeren.d.schulze@gmx.de>
2012-11-10 22:47:19 +0000

    adapted to relative paths
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk@1793 9acf5e20-c632-0410-b30d-e55f0e25a569

--- CommonLogic/colore-check.mk
+++ CommonLogic/colore-check.mk
@@ -9,19 +9,16 @@
 
 COLORE_PATH = colore
 
-CLIF_EXCLUDES := colore/complex/dolce/dolce_hets/dolce_hets_tptp.clif \
-colore/complex/dolce/dolce_original/dolce_mereology.clif \
-colore/complex/dolce/dolce_original/dolce_present.clif \
-colore/complex/dolce/dolce_original/dolce_temporary_parthood.clif \
-colore/complex/dolce/dolce_psl/dolce_present_convex.clif
+CLIF_EXCLUDES := \
+colore/complex/dolce/dolce_hets/dolce_hets_tptp.clif \
+colore/complex/process_specification_language/soo/psl-subset-519.clif \
+colore/complex/process_specification_language/soo/soo.clif \
+colore/complex/SUMO/sumo-cl.clif \
+colore/core/mereological_geometry/cmmg.clif
 
 CLIF_SOURCES := $(filter-out $(CLIF_EXCLUDES),$(shell find $(COLORE_PATH) -name \*.clif))
 #XML_SOURCES := $(shell find $(COLORE_PATH) -name \*.xml)
 # xml is currently not supported
-
-CLIF_IMPORTS := $(shell ls -1d $(foreach src,$(CLIF_SOURCES),         \
-                                             $(shell dirname $(src))) \
-                        | sort | uniq | paste -s -d ':')
 
 all: clif
 
@@ -29,8 +26,7 @@ show_imports:
 	@echo $(CLIF_IMPORTS)
 
 %.dummyclif: %.clif
-	@echo "hets -L [...] $<"
-	@hets -L $(CLIF_IMPORTS) $<
+	hets $<
 	touch $@
 
 %.dummyxml: %.xml
